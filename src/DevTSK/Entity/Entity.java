f0f60fcb56d83fe0e798c8c3311f6be0d7c794af
Cosmos <guitarnerd777@gmail.com>
2016-03-10 13:52:01 -0700

    bhj

---
+++ src/DevTSK/Entity/Entity.java
@@ -0,0 +1,30 @@
+package DevTSK.Entity;
+
+import DevTSK.Util.Day;
+
+public abstract class Entity {
+	protected Boolean Gender = false, /*false = Male, true = Female*/ otherBoolean = false, yaBoolean = false;
+	protected int state = 0, kidammount = 0;
+	protected String name = "Empty", altName = "Empty", description = "Empty", mother = "Empty", father = "Empty", DNA = "Empty", flag = "Empty", additionalInfo = "Empty", dna = "NullDNA", imagepath = "null.png", altimagepath = "null.png", colour = "Empty", anotherColour = "Empty";
+	protected String[] kids = null, with = null, marriedTo = null;
+	protected Day birthday;
+
+	public abstract String toString();
+
+	public String getGender() {
+		if (Gender) {
+			return "Female";
+		}
+		return "Male";
+	}
+
+	public abstract String getInfo();
+
+	public abstract String getName();
+
+	public abstract String getAltName();
+
+	public abstract String getImagePath();
+
+	public abstract String getAltImagePath();
+}
---
+++ src/DevTSK/Util/Colour.java
@@ -0,0 +1,16 @@
+package DevTSK.Util;
+
+import java.awt.Color;
+
+public class Colour extends Color {
+	private static final long serialVersionUID = 1L;
+
+	public Colour(int rgb) {
+		super(rgb);
+	}
+	
+	public Colour(int r, int g, int b){
+		super(r, g, b);
+	}
+
+}
